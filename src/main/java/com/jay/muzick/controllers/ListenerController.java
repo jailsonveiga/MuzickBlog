11d7cacae708a2089374f678935df5e651e603e9
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-27 21:11:48 -0400

    Get your data by Id

--- src/main/java/com/jay/muzick/controllers/ListenerController.java
+++ src/main/java/com/jay/muzick/controllers/ListenerController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -31,5 +35,24 @@ public class ListenerController {
         Listener listener = listenerRepository.save(newListener);
 
         return new ResponseEntity<>(listener, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Listener>> getAllListeners() {
+        List<Listener> listeners = listenerRepository.findAll();
+        return new ResponseEntity<>(listeners, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneListener(@PathVariable Long id) {
+//       Listener listener = listenerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+       Optional<Listener> maybeListener = listenerRepository.findById(id);
+
+       if (maybeListener.isEmpty()) {
+           throw new ResponseStatusException((HttpStatus.OK));
+       }
+
+       return new ResponseEntity<>(maybeListener.get(), HttpStatus.OK);
     }
 }
