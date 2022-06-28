1af218480e18cd1dc01631bece2f8b4fb32f546b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-27 21:09:56 -0400

    Get all your data

--- src/main/java/com/jay/muzick/MuzickApplication.java
+++ src/main/java/com/jay/muzick/MuzickApplication.java
@@ -2,23 +2,13 @@ package com.jay.muzick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class MuzickApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuzickApplication.class, args);
 	}
 
-	@GetMapping("")
-	public static String hello(){
-		return "Hello World";
-	}
-	@GetMapping("/helloworld")
-	public static String world(){
-		return "Hello World";
-	}
 }
