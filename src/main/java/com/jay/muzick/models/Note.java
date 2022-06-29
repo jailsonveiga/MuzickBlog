dcee7c898d18cdbb1b4359091fe39f5258bf61c4
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-28 21:56:42 -0400

    Add Note

--- src/main/java/com/jay/muzick/models/Note.java
+++ src/main/java/com/jay/muzick/models/Note.java
@@ -1,4 +1,63 @@
 package com.jay.muzick.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
+import javax.persistence.*;
+
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "listener_id", referencedColumnName = "id") // (name = {variable_id})
+    @JsonIgnoreProperties("age") // include all except listed
+//    @JsonIncludeProperties({"age", "name"}) // exclude all except listed.
+    private Listener listener;
+
+    public Note() {
+    }
+
+    public Note(Long id, String title, String body, Listener listener) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.listener = listener;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Listener getListener() {
+        return listener;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
 }
