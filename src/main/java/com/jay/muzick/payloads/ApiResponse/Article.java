fd8d01a719d8de0994dbfea8e27c95c78a0e9e14
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-05 19:59:10 -0400

    Add Fields

--- src/main/java/com/jay/muzick/payloads/ApiResponse/Article.java
+++ src/main/java/com/jay/muzick/payloads/ApiResponse/Article.java
@@ -1,4 +1,11 @@
 package com.jay.muzick.payloads.ApiResponse;
 
 public class Article {
+    private String source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+
 }
