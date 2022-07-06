042d6eae09a3996e78d4529a570196c6e8ae5408
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-05 20:03:35 -0400

    Add Constructor

--- src/main/java/com/jay/muzick/payloads/ApiResponse/NewsApi.java
+++ src/main/java/com/jay/muzick/payloads/ApiResponse/NewsApi.java
@@ -1,6 +1,19 @@
 package com.jay.muzick.payloads.ApiResponse;
+
+import java.util.List;
+
 public class NewsApi {
     private String status;
     private Integer totalResults;
-    private String[] articles;
+    private List<Article> articles;
+
+    //Constructor
+
+    public NewsApi() {
+    }
+
+    public NewsApi(String status, Integer totalResults) {
+        this.status = status;
+        this.totalResults = totalResults;
+    }
 }
