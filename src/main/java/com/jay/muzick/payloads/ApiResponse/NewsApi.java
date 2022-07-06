edc32d144fed825d1b2efeb924f53a9726eb80d2
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-05 20:04:15 -0400

    Add Getter and Setter

--- src/main/java/com/jay/muzick/payloads/ApiResponse/NewsApi.java
+++ src/main/java/com/jay/muzick/payloads/ApiResponse/NewsApi.java
@@ -16,4 +16,30 @@ public class NewsApi {
         this.status = status;
         this.totalResults = totalResults;
     }
+
+    //Getter and Setter
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
